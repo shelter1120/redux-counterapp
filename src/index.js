9546e4910a3e91e8a0adbc5398ec06eac5195c10
shelter1120 <shelterk1120>
2023-02-23 08:54:32 +0530

    functionality of dispatch action now  not work

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import { Provider } from 'react-redux';
 import store from './components/store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}> <App /></Provider>);
+root.render(<Provider store={store}>
+     <App />
+     </Provider>);
+     
+
